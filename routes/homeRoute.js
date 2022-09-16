2b9ade21f93597e39ec519238caaa34a99cb7215
Ehren-Lewis <89164935+Ehren-Lewis@users.noreply.github.com>
2022-09-16 13:36:35 -0500

    Added the handlebar dynamic rendering

--- routes/homeRoute.js
+++ routes/homeRoute.js
@@ -1,8 +1,33 @@
 const router = require("express").Router();
+const { Discussions, Category, User, UserCategory } = require("../models");
 
 
-router.get("/", (req, res, next) => {
-    res.render("index");
+// fetch('/api/disucssions').then( (response) => response.json()).then( (data) => {
+//     // [{}, {}, 'hello': {}]
+// })
+router.get("/", async (req, res, next) => {
+    try {
+        const discussionData = await Discussions.findAll({
+          include: [{model: User}]
+        });
+// serialize data
+// create map method 
+// get {plain : true} 
+
+        const newData = discussionData.map( ele => ele.get({plain: true}))
+
+        console.log(newData);
+        if (!discussionData) {
+        // res.status(404).json({ message: "discussion not found" });
+        // return;
+        res.render("error");
+        return;
+        }
+        res.render("index", { newData });
+    }
+    catch (err) {
+        res.render("error")
+    }
 });
 
 module.exports = router;
