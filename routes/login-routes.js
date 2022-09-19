2d3d513c84f09df9cafe624030fce3e80c9f1791
Ehren-Lewis <89164935+Ehren-Lewis@users.noreply.github.com>
2022-09-18 20:45:45 -0500

    Added in the cookies for logging in and automatic redirection if the logged in value is true

--- routes/login-routes.js
+++ routes/login-routes.js
@@ -4,7 +4,13 @@ const router = require("express").Router();
 
 
 router.get("/", (req, res, next) => {
+    if (req.session.loggedIn) {
+        res.redirect("/home");
+        return;
+    } else {
+        // console.log('hererger')
     res.render("login");
+    }
 });
 
 module.exports = router;
