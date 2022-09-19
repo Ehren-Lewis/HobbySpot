6ad89d509f61effb31b63a6bf169d4d8538512f2
Martin-Borchert <108767551+ConnerMart@users.noreply.github.com>
2022-09-19 10:54:41 -0600

    reworking jawsdb connection

--- config/connection.js
+++ config/connection.js
@@ -15,7 +15,7 @@ const sequelize = process.env.JAWSDB_URL
       }
     );
 
-// sequelize = new Sequelize(
+// const sequelize = new Sequelize(
 //   process.env.DB_NAME,
 //   process.env.DB_USER,
 //   process.env.DB_PASSWORD,
